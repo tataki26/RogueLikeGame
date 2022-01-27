17521ed5cfe40ef18e0eab45e5df9e010c4f47a3
tataki26 <72514622+tataki26@users.noreply.github.com>
2022-01-28 00:30:16 +0900

    Create map and printAll function to show the moving of the character

--- RogueLikeGame/RogueLikeGame.cpp
+++ RogueLikeGame/RogueLikeGame.cpp
@@ -1,20 +1,102 @@
-﻿// RogueLikeGame.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include<cstdlib> // = <stdlib.h>
+#include<Windows.h>
+using namespace std;
 
-#include <iostream>
+// 지도 배열
+char map[5][6] = {
+    "#####",
+    "#   #",
+    "# # #",
+    "#   #",
+    "#####"
+};
+
+// 주인공 좌표
+int mainY = 1;
+int mainX = 1;
+
+void printAll()
+{
+    // 화면 클리어 함수
+    // 출력과 클리어를 반복하면 깜빡거리는 효과를 볼 수 있다
+    system("cls");
+
+    // 2중 for문 map 전체 출력
+    for (int y = 0; y < 5; y++)
+    {
+        // null 문자 제외
+        for (int x = 0; x < 5; x++)
+        {
+            if (y == mainY && x == mainX) cout << "@";
+            else cout << map[y][x];
+        }
+        cout << endl;
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    system("color 3F");
+    // 사용자가 종료하기 전까지 무한 반복
+    while(1)
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+
+                if (GetAsyncKeyState(VK_UP) & 0x8001)
+                {
+                    if(map[mainY-1][mainX] != '#') mainY--;
+
+                }
+
+                if (GetAsyncKeyState(VK_DOWN) & 0x8001 != 0)
+                {
+
+                    if (map[mainY + 1][mainX] != '#') mainY++;
+                }
+
+                if (GetAsyncKeyState(VK_LEFT) & 0x8001 != 0)
+                {
+                    if (map[mainY][mainX-1] != '#') mainX--;
+                }
+
+                if (GetAsyncKeyState(VK_RIGHT) & 0x8001 != 0)
+                {
+                    if (map[mainY][mainX+1] != '#') mainX++;
+                }
+
+                Sleep(100);
+
+                printAll();
+               
+            }
+        }
+
+        /*
+        if (GetAsyncKeyState(VK_UP) & 0x8001)
+        {
+            mainY--;
+        }
+
+        if (GetAsyncKeyState(VK_DOWN) & 0x8001 !=0)
+        {
+            mainY++;
+        }
+
+        if (GetAsyncKeyState(VK_LEFT) & 0x8001 != 0)
+        {
+            mainX--;
+        }
+
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8001 != 0)
+        {
+            mainX++;
+        }
+        */
+        
+    }
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
