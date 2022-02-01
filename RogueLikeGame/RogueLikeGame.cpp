98141c8556c192c15718aad817fe4bcc5fb3b31e
tataki26 <72514622+tataki26@users.noreply.github.com>
2022-02-01 13:22:48 +0900

    Update map array and remove useless for loop

--- RogueLikeGame/RogueLikeGame.cpp
+++ RogueLikeGame/RogueLikeGame.cpp
@@ -3,15 +3,30 @@
 #include<Windows.h>
 using namespace std;
 
-const int n = 5;
+const int N = 20;
 
-// 지도 배열
-char map[n][n+1] = {
-    "#####",
-    "# # #",
-    "#^ Y#",
-    "#a M#",
-    "#####"
+char map[N][N + 1] =
+{
+"####################",
+"#  ^^^^^^^^^^^^^^^^#",
+"# ^ MMMMMMMMMMMM   #",
+"#                  #",
+"######             #",
+"#      a           #",
+"#                  #",
+"#   Y              #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#                  #",
+"#           ^    aY#",
+"####################",
 };
 
 // 주인공 좌표
@@ -26,10 +41,10 @@ void printAll()
     system("cls");
 
     // 2중 for문 map 전체 출력
-    for (int y = 0; y < 5; y++)
+    for (int y = 0; y < N; y++)
     {
         // null 문자 제외
-        for (int x = 0; x < 5; x++)
+        for (int x = 0; x < N; x++)
         {
             if (y == mainY && x == mainX) cout << "@";
             else cout << map[y][x];
@@ -46,90 +61,83 @@ int main()
 {
     system("color 3F");
     // 사용자가 종료하기 전까지 무한 반복
-    while(1)
+    while (1)
     {
-        for (int y = 0; y < 5; y++)
+        printAll();
+
+        if (GetAsyncKeyState(VK_UP) & 0x8001)
         {
-            for (int x = 0; x < 5; x++)
-            {
+            if (map[mainY - 1][mainX] == '^') hp -= 10;
+            if (map[mainY - 1][mainX] != '#') mainY--;
 
-                if (GetAsyncKeyState(VK_UP) & 0x8001)
-                {
-                    if (map[mainY - 1][mainX] == '^') hp -= 10;
-                    if(map[mainY-1][mainX] != '#') mainY--;
-
-                }
-
-                else if (GetAsyncKeyState(VK_DOWN) & 0x8001 != 0)
-                {
-                    if (map[mainY + 1][mainX] == '^') hp -= 10;
-                    if (map[mainY + 1][mainX] != '#') mainY++;
- 
-                }
-
-                else if (GetAsyncKeyState(VK_LEFT) & 0x8001 != 0)
-                {
-                    if (map[mainY][mainX - 1] == '^') hp -= 10;
-                    if (map[mainY][mainX-1] != '#') mainX--;
-                    
-                }
-
-                else if (GetAsyncKeyState(VK_RIGHT) & 0x8001 != 0)
-                {
-                    if (map[mainY][mainX + 1] == '^') hp -= 10;
-                    if (map[mainY][mainX+1] != '#') mainX++; 
-
-                }
-
-                if (map[mainY][mainX] == 'M')
-                {
-                    printAll();
-                    for (int i = 0; i < 5; i++)
-                    {
-                        system("color 3F");
-                        Sleep(30);
-                        system("color 0F");
-                        Sleep(70);
-                    }
-                    system("color 0F");
-                    Sleep(300);
-                    system("cls");
-                    cout << "\n\n\n\n\tGAME OVER\n\n\n\n\n";
-                    return 0;
-                }
-
-                if (map[mainY][mainX] == 'Y')
-                {
-                    printAll();
-                    for (int i = 0; i < 5; i++)
-                    {
-                        system("color 3F");
-                        Sleep(30);
-                        system("color 1F");
-                        Sleep(70);
-                    }
-                    system("color 1F");
-                    Sleep(300);
-                    system("cls");
-                    cout << "\n\n\n\n\t  You Win!!!\n\n";
-                    cout << "\tYour Score: " << hp<<"\n\n\n\n";
-                    return 0;
-                }
-
-                if (map[mainY][mainX] == 'a')
-                {
-                    hp = 100;
-                    map[mainY][mainX] = ' ';
-                    
-                }
-
-                Sleep(100);
-
-                printAll();
-               
-            }
         }
-        
-    }
 
+        else if (GetAsyncKeyState(VK_DOWN) & 0x8001 != 0)
+        {
+            if (map[mainY + 1][mainX] == '^') hp -= 10;
+            if (map[mainY + 1][mainX] != '#') mainY++;
+
+        }
+
+        else if (GetAsyncKeyState(VK_LEFT) & 0x8001 != 0)
+        {
+            if (map[mainY][mainX - 1] == '^') hp -= 10;
+            if (map[mainY][mainX - 1] != '#') mainX--;
+
+        }
+
+        else if (GetAsyncKeyState(VK_RIGHT) & 0x8001 != 0)
+        {
+            if (map[mainY][mainX + 1] == '^') hp -= 10;
+            if (map[mainY][mainX + 1] != '#') mainX++;
+
+        }
+
+        if (map[mainY][mainX] == 'M' || hp == 0)
+        {
+            printAll();
+            for (int i = 0; i < 5; i++)
+            {
+                system("color 3F");
+                Sleep(30);
+                system("color 0F");
+                Sleep(70);
+            }
+            system("color 0F");
+            Sleep(300);
+            system("cls");
+            cout << "\n\n\n\n\tGAME OVER\n\n\n\n\n";
+            return 0;
+        }
+
+        if (map[mainY][mainX] == 'Y')
+        {
+            printAll();
+            for (int i = 0; i < 5; i++)
+            {
+                system("color 3F");
+                Sleep(30);
+                system("color 1F");
+                Sleep(70);
+            }
+            system("color 1F");
+            Sleep(300);
+            system("cls");
+            cout << "\n\n\n\n\t  You Win!!!\n\n";
+            cout << "\tYour Score: " << hp << "\n\n\n\n";
+            return 0;
+        }
+
+        if (map[mainY][mainX] == 'a')
+        {
+            hp = 100;
+            map[mainY][mainX] = ' ';
+
+        }
+
+        Sleep(100);
+
+        printAll();
+
+    }
 }
